fbdd722e52b296091835536415855fbdda52a0f7
KevinMieses <93011588+KevinMieses@users.noreply.github.com>
2022-02-14 17:58:18 -0600

    Add files via upload

---
+++ cypress/integration/Cy.fixture.spec.js.js
@@ -0,0 +1,30 @@
+describe ('Search elements',()=>{
+
+ beforeEach(()=>{
+   cy.visit('/')   
+})
+
+it('Search for element with multiple results',()=>{
+
+    cy.search('dress')
+ 
+cy.fixture('searchResult').then((searchResult)=>{
+   cy.get(searchResult.title).should('contain','dress');
+})
+})
+it('Search for elements with no results',()=>{
+    
+    cy.search('qwerty')
+    cy.fixture('index').then((index)=>{
+    cy.get(index.searchBox).type('qwerty');
+    cy.get(index.searchButton).click();
+})
+
+cy.fixture('searchResult').then((searchResult)=>{
+cy.get(searchResult.alert).should('contain','No results were found your search');
+
+
+})
+})
+
+})
