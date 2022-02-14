9054250638e95b5d86a21b7a1c440b30684cc942
KevinMieses <93011588+KevinMieses@users.noreply.github.com>
2022-02-14 17:57:35 -0600

    Add files via upload

---
+++ cypress/integration/2.spec.js.js
@@ -0,0 +1,16 @@
+describe('Log in', ()=>{
+
+    it('Log in page of examples',()=>{
+    
+        cy.visit('https://www.saucedemo.com')
+        cy.get('[data-test="username"]').type('standard_user')
+        cy.get('[data-test="password"]').type('secret_sauce')
+        cy.get('[data-test="login-button"]').click()
+    
+    
+    })
+    
+    
+    
+    
+    })
